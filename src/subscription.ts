0693ee874142e51cddc606c82763c625783c6451
Moralts <instars2233@outlook.com>
2026-03-11 21:30:46 +0800

    feat: 更新规则信息

--- src/subscription.ts
+++ src/subscription.ts
@@ -4,12 +4,12 @@ import categories from './categories';
 import globalGroups from './globalGroups';
 
 export default defineGkdSubscription({
-  id: 233,
-  name: 'Subscription',
-  version: 0,
-  author: 'author',
+  id: 32767,
+  name: 'Moralts 的 GKD 规则',
+  version: 1,
+  author: 'Moralts',
   checkUpdateUrl: './gkd.version.json5',
-  supportUri: 'https://github.com/gkd-kit/subscription-template',
+  supportUri: 'https://github.com/Moralts/GKD_subscription',
   categories,
   globalGroups,
   apps: await batchImportApps(`${import.meta.dirname}/apps`),
