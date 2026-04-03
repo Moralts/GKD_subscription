a2be1eb85bff7a01082529105351d4eb040b1147
Moralts <instars2233@outlook.com>
2026-04-03 23:06:25 +0800

    feat: 适配新的 KOOK-开屏广告 规则

--- src/apps/cn.kaiheila.ts
+++ src/apps/cn.kaiheila.ts
@@ -20,6 +20,13 @@ export default defineGkdApp({
             '@TextView[text^="跳过"][visibleToUser=true][right>getPrev(4).width.div(2)][top<getPrev(4).height.div(2)] <n RelativeLayout <n FrameLayout <n FrameLayout < [vid="ad_container"] <<n [parent=null]',
           snapshotUrls: 'https://i.gkd.li/i/25939059',
         },
+        {
+          key: 1,
+          action: 'click',
+          matches:
+            '@View[visibleToUser=true][clickable=true][right>getPrev(2).width.div(2)][top<getPrev(2).height.div(2)] <n FrameLayout < [vid="fl_splash"] <<n [parent=null]',
+          snapshotUrls: 'https://i.gkd.li/i/25939059',
+        },
       ],
     },
   ],
